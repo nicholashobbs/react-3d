70061250de816b3d2f0b56162de97d6b19d1042c
Nicholas Hobbs <3465766+nicholashobbs@users.noreply.github.com>
2023-02-28 09:54:39 -0700

    add files

---
+++ backend/db/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Syed Afzal
+ * mongoose is used to connect to the database
+ * and also to handle the connection errors
+ * and retry the connection
+ */
+const mongoose = require("mongoose");
+
+exports.connect = (app) => {
+  const options = {
+    useNewUrlParser: true,
+    autoIndex: false, // Don't build indexes
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+  };
+
+  const connectWithRetry = () => {
+    mongoose.Promise = global.Promise;
+    console.log("MongoDB connection with retry");
+    mongoose
+      .connect(process.env.MONGODB_URI, options)
+      .then(() => {
+        console.log("MongoDB is connected");
+        app.emit("ready");
+      })
+      .catch((err) => {
+        console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
+        setTimeout(connectWithRetry, 2000);
+      });
+  };
+  connectWithRetry();
+};
